3c216e7883e47437b87a84e2db304eeb65d9a624
NaoyukiG <naoyukineo@gmail.com>
2022-08-03 17:13:01 +0900

    ルートパスのルーティングの記述を実装

--- app/controllers/mouses_controller.rb
+++ app/controllers/mouses_controller.rb
@@ -1,2 +1,6 @@
 class MousesController < ApplicationController
+
+  def index
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'mouses#index'
+  resources :mouses, only: [:index]
 end
