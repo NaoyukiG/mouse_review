bd5fe85c08983ba95570eafb6f90ebc03e52c269
NaoyukiG <naoyukineo@gmail.com>
2022-08-03 16:58:06 +0900

    管理者ユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  nickname: "管理者",
+  email: "admin@email",
+  password: "admin123",
+  password_confirmation: "admin123",
+  admin: true)
