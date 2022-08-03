b6625f7594cd93a8d7533369cd0ecf6a1cb768df
NaoyukiG <naoyukineo@gmail.com>
2022-08-03 18:12:53 +0900

    sanitizerメソッドでのストロングパラメーターの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    params.require(:user).permit(:nickname, :email, :password)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
 end
