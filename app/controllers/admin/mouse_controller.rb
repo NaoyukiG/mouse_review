4c57ce5df2fdd0c345528e363fc7d3cb43e1470c
NaoyukiG <naoyukineo@gmail.com>
2022-08-05 17:33:45 +0900

    mouseコントローラーへの記述

--- app/controllers/admin/mouse_controller.rb
+++ app/controllers/admin/mouse_controller.rb
@@ -1,2 +1,13 @@
 class Admin::MouseController < ApplicationController
+  before_action :if_not_admin
+
+  def new
+    @mouse = Mouse.new
+
+  private
+
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
 end
