92e76ea40889421f411fdf078a2c2bf43997381f
NaoyukiG <naoyukineo@gmail.com>
2022-08-05 17:57:41 +0900

    mouseモデルにてマイグレーションの実行

--- db/migrate/20220805083324_create_mice.rb
+++ db/migrate/20220805083324_create_mice.rb
@@ -1,7 +1,17 @@
 class CreateMice < ActiveRecord::Migration[6.0]
   def change
-    create_table :mice do |t|
+    create_table :mouses do |t|
 
+      t.string :name,           null: false
+      t.integer :maker_id,      null: false
+      t.integer :connection_id, null: false
+      t.integer :symmetry_id,   null: false
+      t.string :weight,         null: false
+      t.string :string,         null: false
+      t.integer :software_id,   null: false
+      t.text :software_url
+      t.text :order_url,        null: false
+      t.references :user,       null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_074919) do
+ActiveRecord::Schema.define(version: 2022_08_05_083324) do
+
+  create_table "mouses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "maker_id", null: false
+    t.integer "connection_id", null: false
+    t.integer "symmetry_id", null: false
+    t.string "weight", null: false
+    t.string "string", null: false
+    t.integer "software_id", null: false
+    t.text "software_url"
+    t.text "order_url", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_mouses_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +43,5 @@ ActiveRecord::Schema.define(version: 2022_08_03_074919) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "mouses", "users"
 end
