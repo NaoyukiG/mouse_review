b74c2d88317b3ac3f07c286989045c439ae41f74
NaoyukiG <naoyukineo@gmail.com>
2022-08-09 19:20:00 +0900

    activehashを用いた入力欄の実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,11 @@
 class Review < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grip_type
+  belongs_to :play_game
   belongs_to :user
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
+    validates :grip_type_id
+    validates :play_game_id
+  end
 end
