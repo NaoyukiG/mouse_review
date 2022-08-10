7d0ba096839953e2b87bec29879abff0810fdd15
NaoyukiG <naoyukineo@gmail.com>
2022-08-10 13:50:58 +0900

    formオブジェクトモデルへバリデーションを記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,13 +6,5 @@ class Review < ApplicationRecord
   has_many :review_mouses
   has_many :mouses, through: :review_mouses
 
-  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
-    validates :grip_type_id
-    validates :play_game_id
-  end
-
-  with_options presence: true do
-    validates :rating, format: { with: /\A[1-5]\z/, message: "is invalid. Input number from 1 to 5 in half-width.", allow_blank: true }
-    validates :usability
-  end
+  
 end
