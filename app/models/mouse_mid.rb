f515339b102c5aa44540fe931a2c303ce6e118c8
NaoyukiG <naoyukineo@gmail.com>
2022-08-10 13:41:09 +0900

    mouse_midモデルにて、フォームオブジェクトを実装

---
+++ app/models/mouse_mid.rb
@@ -0,0 +1,7 @@
+class MouseMid
+  include ActiveModel::module
+  attr_accessor :grip_type_id, :play_game_id, :rating, :usability, :user_id, :mouse_id
+
+
+  end
+end
--- app/models/review_mouse.rb
+++ app/models/review_mouse.rb
@@ -1,4 +1,7 @@
 class ReviewMouse < ApplicationRecord
   belongs_to :mouse
   belongs_to :review
+
+  validates :mouse_id
+  validates :review_id
 end
