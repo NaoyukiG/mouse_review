f9030a504d6d2fa63d2d2ac4cf9eb439d4855637
NaoyukiG <naoyukineo@gmail.com>
2022-08-20 20:27:57 +0900

    不要なモデルからreviewモデルへのデータ処理を変更する記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,10 @@ class ReviewsController < ApplicationController
   before_action :if_admin
 
   def create
-    @mouse_mid = MouseMid.new(review_params)
-    @mouse = Mouse.find(@mouse_mid.mouse_id)
-    if @mouse_mid.valid?
-      @mouse_mid.save
+    @review = Review.new(review_params)
+    @mouse = Mouse.find(@review.mouse_id)
+    if @review.valid?
+      @review.save
       redirect_to mouse_path(@mouse.id)
     else
       render template: "mouses/show"
@@ -24,7 +24,7 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.require(:mouse_mid).permit(:grip_type_id, :play_game_id, :rating, :usability).merge(user_id: current_user.id, mouse_id: params[:mouse_mid][:mouse_id])
+    params.require(:review).permit(:grip_type_id, :play_game_id, :rating, :usability, :mouse_id).merge(user_id: current_user.id)
   end
 
   def if_admin
